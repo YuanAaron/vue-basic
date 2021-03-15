59acc579f72e22d94428c64bba218c8ce42924f7
yuanwei <yuanwei@kexinjinke.com>
2021-03-15 15:46:47 +0800

    将index2.html中的代码移到myapp中

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+  <div id=app>
+        {{msg}}
+        <div>
+            <input type="text" v-model="info">
+            <button v-on:click="handlerClick">添加</button>
+            <ul>
+                <todo-item v-for="item in list" :key="item" :item="item"></todo-item>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    TodoItem
+  },
+  data() {
+      return {
+          msg: "hello vue!",
+          info: '',
+          list: []
+      }
+  },
+  methods: {
+      handlerClick() {
+          this.list.push(this.info);
+          this.info='';
+      }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
