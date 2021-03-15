5f50ce60da9e479f502837806f259459602172b4
yuanwei <yuanwei@kexinjinke.com>
2021-03-15 16:25:53 +0800

    添加复选框

--- myapp/src/components/TodoItem.vue
+++ myapp/src/components/TodoItem.vue
@@ -1,12 +1,18 @@
 <template class="item">
     <li  class="item">
+        <input type="checkbox" v-model="checked">
         <slot name="item"></slot>
     </li>
 </template>
 
 <script>
 export default {
-    props: ['item']
+    props: ['item'],
+    data () {
+        return {
+            checked: false
+        }
+    }
 }
 </script>
 
